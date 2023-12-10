9e1fc49c86aa732122939227aef23521fa892486
Section80 <jaewon0833@naver.com>
2023-12-10 19:57:17 +0900

    recommand 추가

---
+++ Logic/api_helper.py
@@ -0,0 +1,157 @@
+import requests
+import os
+from datetime import date
+import json
+from enum import Enum
+
+class EWeather(Enum):
+    spring = 0
+    summer = 1
+    fall = 2
+    winter = 3
+
+class Position:
+    def __init__(self, nx: int, ny: int): 
+        self.nx = nx
+        self.ny = ny
+
+# register locations by nx, ny here
+g_position_map = {
+    "seoul": Position(60, 120),
+    "default": Position(60, 120),
+}
+
+WEATHER_SECRET = os.environ["WEATHER_SECRET"]
+
+HASURA_ENDPOINT = "https://fmowl.narumir.io/v1/graphql"
+HASURA_SECRET = os.environ["HASURA_SECRET"]
+USER_CLOSET_QUERY = '''
+    query MyQuery {
+    closet(where: {user_id: {_eq: "%s"}, season: {_eq: "%s"}}) {
+    clothes_id
+    color
+    is_upper
+  }
+}''' 
+
+class EUpperolor(Enum):
+    navy = 0
+    green = 1
+    yellow = 2
+    blue = 3
+    beige = 4
+    red = 5
+
+class ELowerColor(Enum): 
+    light_blue = 0
+    dark_blue = 1
+    black = 2
+    beige = 3
+    cream = 4
+    khaki = 5
+# 색 조합표에서 쌍 동그라미, 동그라미만 채택
+g_color_match_map = {
+    EUpperolor.navy.name: { 
+        ELowerColor.light_blue.name, 
+        ELowerColor.black.name, 
+        ELowerColor.beige.name, 
+        ELowerColor.cream.name, 
+        ELowerColor.khaki.name 
+    }, 
+    EUpperolor.green.name: { 
+        ELowerColor.light_blue.name, 
+        ELowerColor.dark_blue.name,
+        ELowerColor.black.name, 
+        ELowerColor.beige.name 
+    }, 
+    EUpperolor.yellow.name: { 
+        ELowerColor.dark_blue.name,
+        ELowerColor.black.name, 
+        ELowerColor.beige.name, 
+        ELowerColor.khaki.name
+    }, 
+    EUpperolor.blue.name: { 
+        ELowerColor.light_blue.name,
+        ELowerColor.dark_blue.name,
+        ELowerColor.black.name
+    }, 
+    EUpperolor.beige.name: { 
+        ELowerColor.light_blue.name,
+        ELowerColor.dark_blue.name,
+        ELowerColor.black.name, 
+        ELowerColor.cream.name,
+        ELowerColor.khaki.name
+    }, 
+    EUpperolor.red.name: { 
+        ELowerColor.light_blue.name,
+        ELowerColor.black.name, 
+        ELowerColor.beige.name,
+        ELowerColor.cream.name
+    }, 
+}
+
+class UpperCloth: 
+    def __init__(self, id: int, color: EUpperolor):
+        self.id = id
+        self.color = color
+
+class LowerCloth: 
+    def __init__(self, id: int, color: ELowerColor):
+        self.id = id
+        self.color = color
+        
+class MatchedClothes: 
+    def __init__(self, upper: UpperCloth, lower: LowerCloth):
+        self.upper = upper
+        self.lower = lower
+
+def get_weather_by_location(location: str) -> EWeather:
+    position : Position = None
+    if location in g_position_map:
+        position = g_position_map[location]
+    else:
+        position = g_position_map["default"]
+
+    # call wheather API
+    today = date.today()
+    serviceKey = WEATHER_SECRET
+    pageNo = "1"
+    numOfRows = "10"
+    dataType = "JSON"
+    baseDate = today.strftime("%Y%m%d")
+    baseTime = today.strftime("%H%M")
+    nx = position.nx
+    ny = position.ny
+    url = f"http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey={serviceKey}&pageNo={pageNo}&numOfRows={numOfRows}&dataType={dataType}&base_date={baseDate}&base_time={baseTime}&nx={nx}&ny={ny}"
+    res = requests.get(url=url)
+
+    json_res = json.loads(res.text)
+
+    temperature = 0.0
+    items = json_res["response"]["body"]["items"]["item"]
+    for item in items:
+        if item["category"] == "T1H":
+            temperature = float(item["obsrValue"])
+
+    if temperature <= 0.0:  
+        return EWeather.winter
+    elif temperature <= 10:
+        return EWeather.fall
+    elif temperature <= 20:
+        return EWeather.spring
+    else:
+        return EWeather.summer
+
+def get_user_closet(user_id: str, weather: EWeather):
+    # print(f"user_closet: {user_id} weather: {weather.name}", end="\n")
+
+    headers = { "content-type": "application/json", "x-hasura-admin-secret": HASURA_SECRET }
+    json_param = { "query": USER_CLOSET_QUERY % ( user_id, weather.name )}
+
+    res_str = requests.post(HASURA_ENDPOINT, headers=headers, json=json_param)
+
+    print(res_str.text, end="\n")
+
+    res_json = json.loads(res_str.text)
+
+    return res_json["data"]["closet"]
--- Logic/main.py
+++ Logic/main.py
@@ -3,12 +3,21 @@ import requests
 import os
 import json
 import jwt
+from data_model import RecommandRequest
+from fastapi import FastAPI, HTTPException
+from jwt.exceptions import InvalidTokenError
+from api_helper import get_weather_by_location, get_user_closet, g_color_match_map, UpperCloth, LowerCloth, MatchedClothes
+from typing import List
+import random
+
+JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_ALOG = os.getenv("JWT_ALOG")
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return { "msg": "auth_root" }
+    return { "msg": "logic_root" }
 
 TEST_QUERY = '''query MyQuery {
   closet(where: {}) {
@@ -21,6 +30,14 @@ TEST_QUERY = '''query MyQuery {
   }
 }
 '''
+
+USER_CLOSET_QUERY = '''query MyQuery {
+  closet(where: {user_id: {_eq: "user_id"}, season: {_eq: "season"}}) {
+    clothes_id
+    color
+    is_upper
+  }
+}'''
 
 @app.get("/hasura_test")
 async def hasura_test():
@@ -49,28 +66,102 @@ async def weather_test():
       res_str = requests.get(url=url1)
     else: 
       params = {
-          "serviceKey": serviceKey, 
-          "pageNo": pageNo, 
-          "numOfRows": numOfRows,
-          "dataType": dataType, 
-          "base_date" : baseDate, 
-          "base_time": baseTime, 
-          "nx": nx, 
-          "ny": ny
+          "serviceKey": os.environ["WEATHER_SECRET"], 
+          "pageNo": "1", 
+          "numOfRows": "10",
+          "dataType": "JSON", 
+          "base_date" : "20231210", 
+          "base_time": "1700", 
+          "nx": "60", 
+          "ny": "120"
         }
       url2 = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
-      res_str =requests.get(url2, params=params)
+      res_str = requests.get(url=url2, params=params)
 
     res_json = json.loads(res_str.text)
 
     return res_json
   
+@app.get("/weather_test2")
+async def weather_test2(): 
+    print("weather_test2", end="\n")
+    weather = get_weather_by_location("seoul")
+
+    return { "weather": weather.name }
+
 @app.get("/json_test")
 async def json_test():
     key = "test_key"
-    jwt_payload = {"user_id": "1234-1234"}
 
-    encoded = jwt.encode(jwt_payload, key=key, algorithm="HS256")
+    location = "SEOUL"
+    user_id = "c025f706-820e-11ee-b962-0242ac120002"
+
+    jwt_payload = {
+        "location": location,
+        "https://hasura.io/jwt/claims": {
+            "x-hasura-default-role": "user",
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-user-id": user_id,
+        },
+    }
+
+    encoded = jwt.encode(
+        jwt_payload,
+        key=key,
+        algorithm="HS256",
+    )
     decoded = jwt.decode(encoded, key=key, algorithms="HS256")
 
     return { "payload: ": jwt_payload, "encoded": encoded, "decoded": decoded }
+
+@app.post("/recommand")
+async def recommand(request: RecommandRequest):
+    key = "test_key"
+    algo = "HS256"
+    if False:
+        key = JWT_SECRET
+        algo = JWT_ALOG
+
+
+    try:
+        decoded = jwt.decode(request.token, key=key, algorithms=algo)
+
+        location = decoded["location"]
+        claims = decoded["https://hasura.io/jwt/claims"]
+        user_id = claims["x-hasura-user-id"]
+
+        weather = get_weather_by_location(location)
+        closet = get_user_closet(user_id, weather)
+
+        upper_clothes : List[UpperCloth] = []
+        lower_clothes : List[LowerCloth] = []
+
+        for cloth in closet: 
+            if cloth["is_upper"]:
+                upper = UpperCloth(cloth["clothes_id"], cloth["color"])
+                upper_clothes.append(upper)
+            else:
+                lower = LowerCloth(cloth["clothes_id"], cloth["color"])
+                lower_clothes.append(lower)
+
+        matched_clothes_list : List[MatchedClothes] = []
+        for upper in upper_clothes:
+            lower_map = g_color_match_map[upper.color]
+            for lower in lower_clothes:
+                if lower.color in lower_map:
+                    matched_clothes_list.append(MatchedClothes(upper, lower))
+        
+        if len(matched_clothes_list) == 0:
+            return { HTTPException(status_code=404, detail="no match found") }
+        else:   # select random from matched list
+            selected = random.choice(matched_clothes_list)
+            
+            return { "upper_id": selected.upper.id, "lower_id":selected.lower.id }
+
+    except InvalidTokenError: 
+        raise HTTPException(status_code=400, detail="Invalid Token")
+    except KeyError as e:
+        raise HTTPException(status_code=400, detail=f"Invalid Key: {e}")
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=f"Exception: {e}")
